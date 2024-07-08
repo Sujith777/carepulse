d0c5bf62f6f2a425cdc80621c4007199919bd1a2
bspraveen17 <billasujith77@gmail.com>
2024-07-08 22:48:01 +0530

    Complete Patient Form

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,9 @@ export default function Home() {
             width={1000}
             className="mb-12 h-10 w-fit"
           />
+
           <PatientForm />
+
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
               © 2024 CarePulse
