e26214ef7d0aadca918a9f30eb0859cad6956b88
bspraveen17 <billasujith77@gmail.com>
2024-07-09 22:10:07 +0530

    Complete Appwrite configuration and patient form

--- lib/validation.ts
+++ lib/validation.ts
@@ -4,7 +4,7 @@ export const UserFormValidation = z.object({
   name: z
     .string()
     .min(2, "Name must be at least 2 characters.")
-    .max(2, "Name must be at most 50 characters."),
+    .max(50, "Name must be at most 50 characters."),
   email: z.string().email("Invalid email address"),
   phone: z
     .string()
